0b6cf92a25b69f8c39a2c8a0094ddad5c06d1c40
Raphael GOISQUE <raph.goisque@gmail.com>
2025-07-17 18:27:40 +0200

    chore: keep Render instance alive (15min -> 5min)

--- scripts/keepAlive.js
+++ scripts/keepAlive.js
@@ -7,7 +7,7 @@ setInterval(() => {
     axios.get(URL)
         .then(() => console.log(`[PING] ${new Date().toISOString()}`))
         .catch(err => console.error("[PING ERROR]", err.message));
-}, 900_000);          // 300 000 ms = 5 min
+}, 300_000);          // 300 000 ms = 5 min
 
 // Ping immédiat (optionnel)
 axios.get(URL).catch(() => {});
